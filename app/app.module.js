36521caae8cbd8bb492fc530fd0f0e315b50828c
Guillaume Vincent <guillaume@oslab.fr>
2015-10-04 16:43:21 +0200

    create display questions pages

--- app/app.module.js
+++ app/app.module.js
@@ -8,14 +8,16 @@
         .config(routesConfig);
 
 
-    routesConfig.$inject = ['$routeProvider', '$locationProvider'];
+    routesConfig.$inject = ['$routeProvider'];
 
-    function routesConfig($routeProvider, $locationProvider) {
+    function routesConfig($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'static/html/components/index/index.html'
+            })
+            .when('/questions/', {
+                templateUrl: 'static/html/components/questions/questions.html'
             });
-        $locationProvider.html5Mode(true);
     }
 
 })();
